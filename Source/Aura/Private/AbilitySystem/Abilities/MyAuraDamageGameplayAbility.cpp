bf4b1ae3fee8def7609b3da33aff0387b6c0a955
Ushal Naidoo <ushal7naidoo@gmail.com>
2024-02-28 23:54:59 +1300

    Debuff Tags

--- Source/Aura/Private/AbilitySystem/Abilities/MyAuraDamageGameplayAbility.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/MyAuraDamageGameplayAbility.cpp
@@ -9,11 +9,8 @@
 void UMyAuraDamageGameplayAbility::CauseDamage(AActor* TargetActor)
 {
 	FGameplayEffectSpecHandle DamageSpecHandle = MakeOutgoingGameplayEffectSpec(DamageEffectClass, 1.f);
-	for (TTuple<FGameplayTag, FScalableFloat> Pair : DamageTypes)
-	{
-		const float ScaledDamage = Pair.Value.GetValueAtLevel(GetAbilityLevel());
-		UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(DamageSpecHandle, Pair.Key, ScaledDamage);
-	}
+	const float ScaledDamage = Damage.GetValueAtLevel(GetAbilityLevel());
+	UAbilitySystemBlueprintLibrary::AssignTagSetByCallerMagnitude(DamageSpecHandle, DamageType, ScaledDamage);
 	GetAbilitySystemComponentFromActorInfo()->ApplyGameplayEffectSpecToTarget(*DamageSpecHandle.Data.Get(), UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor));
 }
 
@@ -26,10 +23,4 @@ FTaggedMontage UMyAuraDamageGameplayAbility::GetRandomTaggedMontageFromArray(con
 	}
 
 	return FTaggedMontage();
-}
-
-float UMyAuraDamageGameplayAbility::GetDamageByDamageType(float InLevel, const FGameplayTag& DamageType)
-{
-	checkf(DamageTypes.Contains(DamageType), TEXT("GameplayAbilit [%s] does not contain DamageType [%s]"), *GetNameSafe(this), *DamageType.ToString());
-	return DamageTypes[DamageType].GetValueAtLevel(InLevel);
 }
