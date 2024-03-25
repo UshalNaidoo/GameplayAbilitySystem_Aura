eac84d078f8019fb771db840292b3fd21b0d1d6b
Ushal Naidoo <ushal7naidoo@gmail.com>
2024-03-25 13:19:26 +1300

    Map Name Field Notify

--- Source/Aura/Private/Game/AuraGameModeBase.cpp
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -40,3 +40,9 @@ void AAuraGameModeBase::DeleteSlot(const FString& SlotName, int32 SlotIndex)
 		UGameplayStatics::DeleteGameInSlot(SlotName, SlotIndex);
 	}
 }
+
+void AAuraGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+	Maps.Add(DefaultMapName, DefaultMap);
+}
